1bfb644cd84522acf4ea15d15c07e7307b92f63a
Bryan Rodríguez <bryanrodriguezmurillo@outlook.com>
2020-07-10 20:55:29 -0600

    Cambios estructura app Bryan

--- server.R
+++ server.R
@@ -7,9 +7,163 @@ historico_dolar <- readRDS("historico_dolar.RDS")
 grafico_casos <- readRDS("grafico_casos.RDS")
 grafico_hospitalizados <- readRDS("grafico_hospitalizados.RDS")
 grafico_provincia <- readRDS("grafico_provincia.RDS")
+datos_economicos <- data.frame(x = seq(50),
+                               y = rnorm(50, 10, 3),
+                               z = rnorm(50, 11, 2),
+                               w = rnorm(50, 9, 2))
+datos_covid <- data.frame(x = seq(50),
+                          y = rnorm(50, 10, 3),
+                          z = rnorm(50, 11, 2),
+                          w = rnorm(50, 9, 2))
 
 #server
 shinyServer(function(input, output, session){
+  # Tab Resumen: columna Variables economicas --------------------------------------------------------------------------
+  
+  # Value boxes Indicadores Venta dolar --------------------------------------------------------------------------
+  
+  output$precio_venta <- renderValueBox({
+    valueBox(value = 10, 
+             subtitle = "Precio en colones", 
+             icon = icon("dollar-sign"),
+             color = "teal")
+  })
+  
+  output$cambio_venta <- renderValueBox({
+    valueBox(value = 10, 
+             subtitle = "Tasa de cambio %", 
+             icon = icon("arrows-alt-v"),
+             color = "teal")
+  })
+  
+  # Value boxes Indicadores Compra dolar --------------------------------------------------------------------------
+  
+  output$precio_compra <- renderValueBox({
+    valueBox(value = 10, 
+             subtitle = "Precio en colones", 
+             icon = icon("arrows-alt-v"),
+             color = "teal")
+  })
+  
+  output$cambio_compra <- renderValueBox({
+    valueBox(value = 10, 
+             subtitle = "Tasa de cambio %", 
+             icon = icon("arrows-alt-v"),
+             color = "teal")
+  })
+  
+  # Value boxes Indicadores Inflacion --------------------------------------------------------------------------
+  
+  output$valor_inflacion <- renderValueBox({
+    valueBox(value = 10, 
+             subtitle = "Inflacion % anual", 
+             icon = icon("arrows-alt-v"),
+             color = "teal")
+  })
+  
+  output$cambio_inflacion <- renderValueBox({
+    valueBox(value = 10, 
+             subtitle = "Tasa de cambio %", 
+             icon = icon("arrows-alt-v"),
+             color = "teal")
+  })
+  
+  # Value boxes Indicadores Importaciones --------------------------------------------------------------------------
+  
+  output$valor_importaciones <- renderValueBox({
+    valueBox(value = 10, 
+             subtitle = "Importaciones en millones de $", 
+             icon = icon("arrows-alt-v"),
+             color = "teal")
+  })
+  
+  output$cambio_importaciones <- renderValueBox({
+    valueBox(value = 10, 
+             subtitle = "Tasa de cambio %", 
+             icon = icon("arrows-alt-v"),
+             color = "teal")
+  })
+  
+  # Value boxes Indicadores Exportaciones --------------------------------------------------------------------------
+  
+  output$valor_exportaciones <- renderValueBox({
+    valueBox(value = 10, 
+             subtitle = "Exportaciones en millones de $", 
+             icon = icon("arrows-alt-v"),
+             color = "teal")
+  })
+  
+  output$cambio_exportaciones <- renderValueBox({
+    valueBox(value = 10, 
+             subtitle = "Tasa de cambio %", 
+             icon = icon("arrows-alt-v"),
+             color = "teal")
+  })
+  
+  # Tab Resumen: columna datos COVID --------------------------------------------------------------------------
+  
+  # Value boxes Casos nuevos --------------------------------------------------------------------------
+  
+  output$casos_nuevos <- renderValueBox({
+    valueBox(value = 10, 
+             subtitle = "Casos nuevos", 
+             icon = icon("arrows-alt-v"),
+             color = "maroon")
+  })
+  
+  output$cambio_nuevos <- renderValueBox({
+    valueBox(value = 10, 
+             subtitle = "Tasa de cambio % casos nuevos", 
+             icon = icon("arrows-alt-v"),
+             color = "maroon")
+  })
+  
+  # Value box Hospitalizados --------------------------------------------------------------------------
+  
+  output$valor_hospitalizados <- renderValueBox({
+    valueBox(value = 10, 
+             subtitle = "Pacientes hospitalizados", 
+             icon = icon("arrows-alt-v"),
+             color = "maroon")
+  })
+  
+  # Value box UCI --------------------------------------------------------------------------
+  
+  output$valor_uci <- renderValueBox({
+    valueBox(value = 10, 
+             subtitle = "Pacientes en UCI", 
+             icon = icon("arrows-alt-v"),
+             color = "maroon")
+  })
+  
+  # Value box Recuperados --------------------------------------------------------------------------
+  
+  output$valor_recuperados <- renderValueBox({
+    valueBox(value = 10, 
+             subtitle = "Personas recuperadas", 
+             icon = icon("arrows-alt-v"),
+             color = "maroon")
+  })
+  
+  # Tab Resumen: graficos series de tiempo --------------------------------------------------------------------------
+  
+  # Grafico variable economica --------------------------------------------------------------------------
+  
+  output$graf_var <- renderEcharts4r({
+    
+    datos_economicos %>% 
+      e_charts(x) %>% 
+      e_line(z) %>% 
+      e_area(w) %>% 
+      e_theme("infographic") %>% 
+      e_datazoom()
+    
+  })
+  
+  
+  
+  
+  # App anterior --------------------------------------------------------------------------
   output$compra_dolar <- renderValueBox({
     valueBox(datos[nrow(datos),2],
              tags$p("Tipo de cambio compra",
--- ui.R
+++ ui.R
@@ -9,19 +9,124 @@ library(echarts4r)
 #Interfaz de usuario
 
 shinyUI(
-  dashboardPage(skin = "red",
-    dashboardHeader(title = "Diagnostico economico CR",
+  dashboardPage(skin = "blue",
+    dashboardHeader(title = "Indicadores económicos y COVID-19 de CR",
                     titleWidth = 450),
     dashboardSidebar(
       sidebarMenu(
         id = "menu",
-        menuItem("General", tabName = "general", icon = icon("chart-line")),
+        menuItem("Resumen", tabName = "resumen", icon = icon("sort-amount-up")),
+        menuItem("Modelos", tabName = "modelos", icon = icon("chart-line")),
+        menuItem("General", tabName = "general"),
         menuItem("Situacion Nacional COVID", tabName = "covid"),
         menuItem("Exportaciones e importaciones", tabName = "expo_impo")
       )
     ),
     dashboardBody(
       tabItems(
+        # Tab Resumen --------------------------------------------------------------------------
+        tabItem("resumen",
+                h3("Indicadores"),
+                selectInput(inputId = "variables", 
+                            label = "Elija la variable macroeconómica a visualizar:", 
+                            choices = c("Tipo de cambio: Venta $",
+                                        "Tipo de cambio: Compra $",
+                                        "Inflación",
+                                        "Importaciones",
+                                        "Exportaciones"
+                                        ),
+                            width = 1000),
+                fluidRow(
+                  # columna Variables economicas --------------------------------------------------------------------------
+                    
+                  column(
+                    width = 6,
+                    box(title = "Datos de variable macroeconomica", 
+                        solidHeader = TRUE,
+                        collapsible = TRUE,
+                        width = NULL,
+                        status = "primary",
+                        
+                        # Indicadores Venta dolar --------------------------------------------------------------------------
+                        
+                        conditionalPanel(
+                          condition = "input.variables == 'Tipo de cambio: Venta $'",
+                          h4("Tipo de cambio: Venta del $"),
+                          valueBoxOutput("precio_venta", width = NULL),
+                          valueBoxOutput("cambio_venta", width = NULL)
+                        ),
+                        
+                        # Indicadores Compra dolar --------------------------------------------------------------------------
+                        
+                        conditionalPanel(
+                          condition = "input.variables == 'Tipo de cambio: Compra $'",
+                          h4("Tipo de cambio: Compra $"),
+                          valueBoxOutput("precio_compra", width = NULL),
+                          valueBoxOutput("cambio_compra", width = NULL)
+                        ),
+                        
+                        # Indicadores Inflacion --------------------------------------------------------------------------
+                        
+                        conditionalPanel(
+                          condition = "input.variables == 'Inflación'",
+                          h4("Inflación comercial"), 
+                          valueBoxOutput("valor_inflacion", width = NULL),
+                          valueBoxOutput("cambio_inflacion", width = NULL)
+                        ),
+                        
+                        # Indicadores Importaciones --------------------------------------------------------------------------
+                        
+                        conditionalPanel(
+                          condition = "input.variables == 'Importaciones'",
+                          h4("Importaciones (CIF)"),
+                          valueBoxOutput("valor_importaciones", width = NULL),
+                          valueBoxOutput("cambio_importaciones", width = NULL)
+                        ),
+                        
+                        # Indicadores Exportaciones --------------------------------------------------------------------------
+                        
+                        conditionalPanel(
+                          condition = "input.variables == 'Exportaciones'",
+                          h4("Exportaciones (FOB)"), 
+                          valueBoxOutput("valor_exportaciones", width = NULL),
+                          valueBoxOutput("cambio_exportaciones", width = NULL)
+                        )
+                    ),
+                  ),
+                  # columna Datos COVID --------------------------------------------------------------------------
+                  
+                  column(
+                    width = 6,
+                    box(title = "Datos del COVID-19", 
+                        solidHeader = TRUE,
+                        collapsible = TRUE,
+                        width = NULL,
+                        status = "primary",
+                        h4("Información al dia de hoy"), 
+                        h5(""),
+                        valueBoxOutput("casos_nuevos", width = NULL),
+                        valueBoxOutput("cambio_nuevos", width = NULL),
+                        valueBoxOutput("valor_hospitalizados", width = NULL),
+                        valueBoxOutput("valor_uci", width = NULL),
+                        valueBoxOutput("valor_recuperados", width = NULL)
+                    )
+                  )
+                ),
+                h3("Series de tiempo"),
+                fluidRow(
+                  column(
+                    width = 12,
+                    box(title = "Grafico de variable economica",
+                        collapsible = TRUE,
+                        solidHeader = TRUE,
+                        width = NULL,
+                        status = "primary",
+                        echarts4rOutput("graf_var")
+                    )
+                  )
+                )
+        ),
+        
         tabItem("general",
           h3("Tipo de cambio al dia de hoy"),
           fluidRow(
